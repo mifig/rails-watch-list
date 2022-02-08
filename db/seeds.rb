5c9e02d6d2507b67581ed70b0ac144b87e9d4ef7
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2022-02-08 16:56:24 +0000

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+require "json"
+
+url_movies = "http://tmdb.lewagon.com/movie/top_rated"
+url_poster_base = "https://image.tmdb.org/t/p/original/"
+
+movies_serialized = URI.open(url_movies).read
+movies = JSON.parse(movies_serialized)
+
+Movie.destroy_all
+
+movies["results"].each do |movie|
+  Movie.create(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "#{url_poster_base}/#{movie["poster_path"]}",
+    rating: movie["vote_average"]
+  )
+end
